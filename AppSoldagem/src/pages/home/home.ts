912c1dbdaacc5e4c317d02f29c2872c956ac2881
Ian Jairo <ijtg.ian@gmail.com>
2019-10-18 23:45:23 -0300

    Pagina Home Configurada

--- AppSoldagem/src/pages/home/home.ts
+++ AppSoldagem/src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+//import { TipoOperacaoPage } from '../tipo-operacao/tipo-operacao';
+
 
 @Component({
   selector: 'page-home',
@@ -7,7 +9,17 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  public home;
+
   constructor(public navCtrl: NavController) {
+    this.home = [
+      {text: "Temos o objetivo de interativamente de calcular valores de metalurgia e estimar custo nos processos de soldagem"},
+      {text: "É só introduzir parâmetros dos chanfros em juntas soldadas e os processos utilizados"}
+    ];
+  }
+
+  onSeguinte() {
+    //this.navCtrl.push(TipoOperacaoPage);
 
   }
 
